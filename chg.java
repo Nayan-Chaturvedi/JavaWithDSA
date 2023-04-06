e33b136040b469e9fa8405ab1a52ea9a734b6348
NayanChaturved <chaturvedinayan5>
2023-04-06 15:21:33 +0530

    It'simple

---
+++ chg.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class stu
+{
+int roll;
+String name;
+static String clg name="x clg";
+
+stu(int x,String y)
+{
+  roll=x;
+  name=y;
+ } 
+void display()
+{
+System.out.println("roll="+roll+"name="+name+"clg name="+x clg);
+}
+static void change()
+{
+clg name="y clg";
+}
+class chg
+{
+public static void main(String args[])
+{
+
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a roll number");
+a=sc.nextInt();
+String b;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter name");
+b=sc.nextInt();
+
+aa.display();
+bb.display();
+
+
+stu. change();
+aa.display();
+bb.display();
+}
+}
